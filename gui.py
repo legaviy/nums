7190c6cb381f25822768ed747d737303c89d13f5
LE7ABiy <93426807+legaviy@users.noreply.github.com>
2021-11-03 12:08:16 +0300

    qwerty

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 from kivy.app import App
 from kivy.uix.behaviors import button
-
+#
 from kivy.uix.button import Button
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
